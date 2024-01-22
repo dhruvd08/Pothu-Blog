8fdd82af5274d7d8da42137242a2c5031d29edbb
Maulik Dave <mauldave@gmail.com>
2024-01-22 10:35:56 +0530

    Remove TODOs from form.py

--- forms.py
+++ forms.py
@@ -13,7 +13,6 @@ class CreatePostForm(FlaskForm):
     submit = SubmitField("Submit Post")
 
 
-# TODO: Create a RegisterForm to register new users
 class RegisterForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
     email = EmailField('Email', validators=[DataRequired()])
@@ -21,14 +20,12 @@ class RegisterForm(FlaskForm):
     submit = SubmitField('Sign me up!')
 
 
-# TODO: Create a LoginForm to login existing users
 class LoginForm(FlaskForm):
     email = EmailField('Email', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Let me in!')
 
 
-# TODO: Create a CommentForm so users can leave comments below posts
 class CommentForm(FlaskForm):
     comment = CKEditorField('Comment', validators=[DataRequired(), Length(max=500)])
     submit = SubmitField('Post')
